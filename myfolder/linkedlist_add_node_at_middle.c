94e8a5a65d873405fc6d988dd26975ab4ab24e6c
Gajjalabharathi16 <149307941+Gajjalabharathi16@users.noreply.github.com>
2025-06-17 20:36:15 +0530

    Update linkedlist_add_node_at_middle.c

--- myfolder/linkedlist_add_node_at_middle.c
+++ myfolder/linkedlist_add_node_at_middle.c
@@ -1,3 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+void list_creation();
+void add_node_at_middle();
+void display();
+struct linked_list
+{
+    int data;
+    struct linked_list *link;
+};
+struct linked_list *head=NULL;
+int main()
+{
+    list_creation();
+    add_node_at_middle();
+    display();
+}
+void list_creation()
+{
+    struct linked_list *newnode=NULL;
+     struct linked_list *temp=NULL;
+     int choice;
+     printf("enter no of nodes\n");
+     scanf("%d",&choice);
+     for(int i=0;i<choice;i++)
+     {
+         newnode=(struct linked_list *)malloc(sizeof(struct linked_list));
+         int value;
+         scanf("%d",&value);
+         newnode->data=value;
+         newnode->link=NULL;
+         if(head==NULL)
+         {
+             head=temp=newnode;
+         }
+         else
+         {
+             temp->link=newnode;
+             temp=newnode;
+         }
      }
 }
 
@@ -28,5 +68,3 @@ void display()
         temp=temp->link;
     }
 }
-                                                                                                          70,1          Bot
-                                                                                                            
