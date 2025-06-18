8e897826b591c6df0c9dd7911db5db3e1ac95c01
Gajjalabharathi16 <149307941+Gajjalabharathi16@users.noreply.github.com>
2025-06-18 11:04:36 +0530

    Update 13.c

--- myfolder/lsp/13.c
+++ myfolder/lsp/13.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+void listFilesRecursively(const char *basePath);
 int main() {
     char basePath[100];
 
